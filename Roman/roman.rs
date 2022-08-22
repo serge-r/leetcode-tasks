9b4198b9f40b8e0b939e191cabc485931f1bb156
Sergei Razukov <zoneofmail@gmail.com>
2022-08-22 20:50:24 +0700

    fix(roman): add int_to_roman rust solution

--- Roman/roman.rs
+++ Roman/roman.rs
@@ -65,3 +65,48 @@ pub fn roman_to_int(str: &str) -> u32 {
     }
     sum
 }
+
+pub fn int_to_roman(num: i32) -> String {
+    const LEN: usize = 13;
+
+    let mut result: String = String::from("");
+    let mut res;
+    let mut num_copy = num;
+    let nums: [i32; LEN] = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let lit = HashMap::from([
+        (1, "I"),
+        (4, "IV"),
+        (5, "V"),
+        (9, "IX"),
+        (10, "X"),
+        (40, "XL"),
+        (50, "L"),
+        (90, "XC"),
+        (100, "C"),
+        (400, "CD"),
+        (500, "D"),
+        (900, "CM"),
+        (1000, "M"),
+    ]);
+
+    for j in 0..LEN {
+        if num_copy > 0 && num_copy < 4000 {
+            res = num_copy / nums[j];
+            if res > 3 {
+                result.push_str(lit[&nums[j]]);
+                result.push_str(lit[&nums[j - 1]]);
+            } else {
+                let mut i = res;
+                while i > 0 {
+                    result = result + lit[&nums[j]];
+                    i -= 1;
+                }
+            }
+            num_copy -= nums[j] * res;
+        } else {
+            break;
+        }
+    }
+
+    result
+}
